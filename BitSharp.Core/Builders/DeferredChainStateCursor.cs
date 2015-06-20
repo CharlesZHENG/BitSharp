7114a5b17be0fdb570961d70f5d2949c4f1cfbf8
Paul <pmlyon@hotmail.ca>
2015-06-20 08:58:59 -0300

    add a deferred chain state cursor, caches utxo access & will allow for simultaneous block validation

---
+++ BitSharp.Core/Builders/DeferredChainStateCursor.cs
@@ -0,0 +1,249 @@
+﻿using BitSharp.Common;
+using BitSharp.Core.Domain;
+using BitSharp.Core.Storage;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitSharp.Core.Builders
+{
+    internal class DeferredChainStateCursor : IChainStateCursor
+    {
+        private readonly IChainStateCursor parent;
+        private readonly ChainedHeader originalChainTip;
+
+        private DeferredDictionary<UInt256, UnspentTx> unspentTxes;
+        private DeferredDictionary<int, IImmutableList<UInt256>> blockSpentTxes;
+        private DeferredDictionary<UInt256, IImmutableList<UnmintedTx>> blockUnmintedTxes;
+
+        private bool changesApplied;
+
+        public DeferredChainStateCursor(IChainStateCursor parent)
+        {
+            if (!parent.InTransaction)
+                throw new ArgumentException("parent");
+
+            this.parent = parent;
+            this.originalChainTip = parent.GetChainTip();
+
+            UnspentOutputCount = parent.UnspentOutputCount;
+            UnspentTxCount = parent.UnspentTxCount;
+            TotalTxCount = parent.TotalTxCount;
+            TotalInputCount = parent.TotalInputCount;
+            TotalOutputCount = parent.TotalOutputCount;
+
+            unspentTxes = new DeferredDictionary<UInt256, UnspentTx>(
+                txHash =>
+                {
+                    UnspentTx unspentTx;
+                    return Tuple.Create(parent.TryGetUnspentTx(txHash, out unspentTx), unspentTx);
+                });
+
+            blockSpentTxes = new DeferredDictionary<int, IImmutableList<UInt256>>(
+                blockHeight =>
+                {
+                    IImmutableList<UInt256> spentTxes;
+                    return Tuple.Create(parent.TryGetBlockSpentTxes(blockHeight, out spentTxes), spentTxes);
+                });
+
+            blockUnmintedTxes = new DeferredDictionary<UInt256, IImmutableList<UnmintedTx>>(
+                blockHash =>
+                {
+                    IImmutableList<UnmintedTx> unmintedTxes;
+                    return Tuple.Create(parent.TryGetBlockUnmintedTxes(blockHash, out unmintedTxes), unmintedTxes);
+                });
+        }
+
+        public void Dispose()
+        {
+            unspentTxes.Dispose();
+            blockSpentTxes.Dispose();
+            blockUnmintedTxes.Dispose();
+        }
+
+        public bool InTransaction
+        {
+            get { throw new NotSupportedException(); }
+        }
+
+        public void BeginTransaction(bool readOnly = false)
+        {
+            throw new NotSupportedException();
+        }
+
+        public void CommitTransaction()
+        {
+            throw new NotSupportedException();
+        }
+
+        public void RollbackTransaction()
+        {
+            throw new NotSupportedException();
+        }
+
+        public IEnumerable<ChainedHeader> ReadChain()
+        {
+            throw new NotSupportedException();
+        }
+
+        public ChainedHeader GetChainTip()
+        {
+            throw new NotSupportedException();
+        }
+
+        public void AddChainedHeader(ChainedHeader chainedHeader)
+        {
+            throw new NotSupportedException();
+        }
+
+        public void RemoveChainedHeader(ChainedHeader chainedHeader)
+        {
+            throw new NotSupportedException();
+        }
+
+        public int UnspentTxCount { get; set; }
+
+        public int UnspentOutputCount { get; set; }
+
+        public int TotalTxCount { get; set; }
+
+        public int TotalInputCount { get; set; }
+
+        public int TotalOutputCount { get; set; }
+
+        public bool ContainsUnspentTx(UInt256 txHash)
+        {
+            return unspentTxes.ContainsKey(txHash);
+        }
+
+        public bool TryGetUnspentTx(UInt256 txHash, out UnspentTx unspentTx)
+        {
+            return unspentTxes.TryGetValue(txHash, out unspentTx);
+        }
+
+        public bool TryAddUnspentTx(UnspentTx unspentTx)
+        {
+            return unspentTxes.TryAdd(unspentTx.TxHash, unspentTx);
+        }
+
+        public bool TryRemoveUnspentTx(UInt256 txHash)
+        {
+            return unspentTxes.TryRemove(txHash);
+        }
+
+        public bool TryUpdateUnspentTx(UnspentTx unspentTx)
+        {
+            return unspentTxes.TryUpdate(unspentTx.TxHash, unspentTx);
+        }
+
+        public IEnumerable<UnspentTx> ReadUnspentTransactions()
+        {
+            throw new NotSupportedException();
+        }
+
+        public bool ContainsBlockSpentTxes(int blockIndex)
+        {
+            return blockSpentTxes.ContainsKey(blockIndex);
+        }
+
+        public bool TryGetBlockSpentTxes(int blockIndex, out IImmutableList<UInt256> spentTxes)
+        {
+            return blockSpentTxes.TryGetValue(blockIndex, out spentTxes);
+        }
+
+        public bool TryAddBlockSpentTxes(int blockIndex, IImmutableList<UInt256> spentTxes)
+        {
+            return blockSpentTxes.TryAdd(blockIndex, spentTxes);
+        }
+
+        public bool TryRemoveBlockSpentTxes(int blockIndex)
+        {
+            return blockSpentTxes.TryRemove(blockIndex);
+        }
+
+        public bool ContainsBlockUnmintedTxes(UInt256 blockHash)
+        {
+            return blockUnmintedTxes.ContainsKey(blockHash);
+        }
+
+        public bool TryGetBlockUnmintedTxes(UInt256 blockHash, out IImmutableList<UnmintedTx> unmintedTxes)
+        {
+            return blockUnmintedTxes.TryGetValue(blockHash, out unmintedTxes);
+        }
+
+        public bool TryAddBlockUnmintedTxes(UInt256 blockHash, IImmutableList<UnmintedTx> unmintedTxes)
+        {
+            return blockUnmintedTxes.TryAdd(blockHash, unmintedTxes);
+        }
+
+        public bool TryRemoveBlockUnmintedTxes(UInt256 blockHash)
+        {
+            return blockUnmintedTxes.TryRemove(blockHash);
+        }
+
+        public void Flush()
+        {
+            throw new NotSupportedException();
+        }
+
+        public void Defragment()
+        {
+            throw new NotSupportedException();
+        }
+
+        public void WarmUnspentTx(UInt256 txHash, Func<Tuple<bool, UnspentTx>> getUnspentTx)
+        {
+            unspentTxes.WarmupValue(txHash, getUnspentTx);
+        }
+
+        public void ApplyChangesToParent()
+        {
+            if (changesApplied)
+                throw new InvalidOperationException();
+
+            var currentChainTip = parent.GetChainTip();
+            if (!(currentChainTip == null && originalChainTip == null) && currentChainTip.Hash != originalChainTip.Hash)
+                throw new InvalidOperationException();
+
+            parent.UnspentOutputCount = UnspentOutputCount;
+            parent.UnspentTxCount = UnspentTxCount;
+            parent.TotalTxCount = TotalTxCount;
+            parent.TotalInputCount = TotalInputCount;
+            parent.TotalOutputCount = TotalOutputCount;
+
+            foreach (var unspentTx in unspentTxes.Updated.Values)
+                if (!parent.TryUpdateUnspentTx(unspentTx))
+                    throw new InvalidOperationException();
+            foreach (var unspentTx in unspentTxes.Added.Values)
+                if (!parent.TryAddUnspentTx(unspentTx))
+                    throw new InvalidOperationException();
+            foreach (var txHash in unspentTxes.Deleted)
+                if (!parent.TryRemoveUnspentTx(txHash))
+                    throw new InvalidOperationException();
+
+            if (blockSpentTxes.Updated.Count > 0)
+                throw new InvalidOperationException();
+            foreach (var spentTxes in blockSpentTxes.Added)
+                if (!parent.TryAddBlockSpentTxes(spentTxes.Key, spentTxes.Value))
+                    throw new InvalidOperationException();
+            foreach (var blockHeight in blockSpentTxes.Deleted)
+                if (!parent.TryRemoveBlockSpentTxes(blockHeight))
+                    throw new InvalidOperationException();
+
+            if (blockUnmintedTxes.Updated.Count > 0)
+                throw new InvalidOperationException();
+            foreach (var unmintedTxes in blockUnmintedTxes.Added)
+                if (!parent.TryAddBlockUnmintedTxes(unmintedTxes.Key, unmintedTxes.Value))
+                    throw new InvalidOperationException();
+            foreach (var blockHeight in blockUnmintedTxes.Deleted)
+                if (!parent.TryRemoveBlockUnmintedTxes(blockHeight))
+                    throw new InvalidOperationException();
+
+            changesApplied = true;
+        }
+    }
+}
---
+++ BitSharp.Core/Builders/DeferredDictionary.cs
@@ -0,0 +1,248 @@
+﻿using BitSharp.Common;
+using BitSharp.Common.ExtensionMethods;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BitSharp.Core.Builders
+{
+    internal class DeferredDictionary<TKey, TValue> : IDisposable
+    {
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        private Dictionary<TKey, TValue> read = new Dictionary<TKey, TValue>();
+        private HashSet<TKey> missing = new HashSet<TKey>();
+        private Dictionary<TKey, TValue> updated = new Dictionary<TKey, TValue>();
+        private Dictionary<TKey, TValue> added = new Dictionary<TKey, TValue>();
+        private HashSet<TKey> deleted = new HashSet<TKey>();
+
+        private readonly Func<TKey, Tuple<bool, TValue>> parentTryGetValue;
+
+        private readonly ReaderWriterLockSlim warmupLock = new ReaderWriterLockSlim();
+
+        public DeferredDictionary(Func<TKey, Tuple<bool, TValue>> parentTryGetValue)
+        {
+            this.parentTryGetValue = parentTryGetValue;
+        }
+
+        public void Dispose()
+        {
+            this.warmupLock.Dispose();
+        }
+
+        public IDictionary<TKey, TValue> Updated { get { return updated; } }
+
+        public IDictionary<TKey, TValue> Added { get { return added; } }
+
+        public ISet<TKey> Deleted { get { return deleted; } }
+
+        public bool ContainsKey(TKey key)
+        {
+            return warmupLock.DoRead(() =>
+            {
+                if (!missing.Contains(key) && !deleted.Contains(key))
+                {
+                    if (read.ContainsKey(key) || updated.ContainsKey(key) || added.ContainsKey(key))
+                        return true;
+
+                    TValue value;
+                    if (TryGetParentValue(key, out value))
+                    {
+                        read.Add(key, value);
+                        return true;
+                    }
+                    else
+                    {
+                        missing.Add(key);
+                        return false;
+                    }
+                }
+
+                return false;
+            });
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            warmupLock.EnterReadLock();
+            try
+            {
+                if (!missing.Contains(key) && !deleted.Contains(key))
+                {
+                    if (read.TryGetValue(key, out value) || updated.TryGetValue(key, out value) || added.TryGetValue(key, out value))
+                        return true;
+
+                    if (TryGetParentValue(key, out value))
+                    {
+                        read.Add(key, value);
+                        return true;
+                    }
+                    else
+                    {
+                        missing.Add(key);
+                        return false;
+                    }
+                }
+
+                value = default(TValue);
+                return false;
+            }
+            finally
+            {
+                warmupLock.ExitReadLock();
+            }
+        }
+
+        public bool TryAdd(TKey key, TValue value)
+        {
+            return warmupLock.DoRead(() =>
+            {
+                if (missing.Contains(key))
+                {
+                    missing.Remove(key);
+                    added.Add(key, value);
+                    return true;
+                }
+                else if (deleted.Contains(key))
+                {
+                    deleted.Remove(key);
+                    updated.Add(key, value);
+                    return true;
+                }
+                else if (read.ContainsKey(key))
+                {
+                    return false;
+                }
+                else if (!added.ContainsKey(key) && !updated.ContainsKey(key))
+                {
+                    TValue existingValue;
+                    if (!TryGetParentValue(key, out existingValue))
+                    {
+                        added.Add(key, value);
+                        return true;
+                    }
+                    else
+                    {
+                        read.Add(key, existingValue);
+                        return false;
+                    }
+                }
+                else
+                    return false;
+            });
+        }
+
+        public bool TryRemove(TKey key)
+        {
+            return warmupLock.DoRead(() =>
+            {
+                TValue ignore;
+
+                if (missing.Contains(key) || deleted.Contains(key))
+                {
+                    return false;
+                }
+                else if (read.ContainsKey(key) || updated.ContainsKey(key) || added.ContainsKey(key) || TryGetParentValue(key, out ignore))
+                {
+                    deleted.Add(key);
+                    read.Remove(key);
+                    updated.Remove(key);
+                    added.Remove(key);
+                    return true;
+                }
+                else
+                    return false;
+            });
+        }
+
+        public bool TryUpdate(TKey key, TValue value)
+        {
+            return warmupLock.DoRead(() =>
+            {
+                TValue ignore;
+
+                if (missing.Contains(key) || deleted.Contains(key))
+                {
+                    return false;
+                }
+                else if (read.ContainsKey(key))
+                {
+                    Debug.Assert(!updated.ContainsKey(key));
+                    Debug.Assert(!added.ContainsKey(key));
+
+                    updated.Add(key, value);
+                    read.Remove(key);
+                    return true;
+                }
+                else if (updated.ContainsKey(key))
+                {
+                    Debug.Assert(!read.ContainsKey(key));
+                    Debug.Assert(!added.ContainsKey(key));
+
+                    updated[key] = value;
+                    return true;
+                }
+                else if (added.ContainsKey(key))
+                {
+                    Debug.Assert(!read.ContainsKey(key));
+                    Debug.Assert(!updated.ContainsKey(key));
+
+                    added[key] = value;
+                    return true;
+                }
+                else if (TryGetParentValue(key, out ignore))
+                {
+                    updated[key] = value;
+                    return true;
+                }
+                else
+                    return false;
+            });
+        }
+
+        public void WarmupValue(TKey key, Func<Tuple<bool, TValue>> getValue)
+        {
+            warmupLock.DoWrite(() =>
+            {
+                if (!read.ContainsKey(key) && !missing.Contains(key) && !updated.ContainsKey(key) && !added.ContainsKey(key) && !deleted.Contains(key))
+                {
+                    var value = getValue();
+                    if (value.Item1)
+                        read.Add(key, value.Item2);
+                    else
+                        missing.Add(key);
+                }
+            });
+        }
+
+        private static DurationMeasure getValueMeasure = new DurationMeasure(sampleCutoff: TimeSpan.FromMinutes(5));
+        private bool TryGetParentValue(TKey key, out TValue value)
+        {
+            var stopwatch = Stopwatch.StartNew();
+            var result = parentTryGetValue(key);
+            stopwatch.Stop();
+
+            if (result.Item1)
+            {
+                getValueMeasure.Tick(stopwatch.Elapsed);
+                //Throttler.IfElapsed(TimeSpan.FromMilliseconds(50), () =>
+                //{
+                //    logger.Info("{0:N3}ms".Format2(getValueMeasure.GetAverage().TotalMilliseconds));
+                //});
+
+                value = result.Item2;
+                return true;
+            }
+            else
+            {
+                value = default(TValue);
+                return false;
+            }
+        }
+    }
+}
