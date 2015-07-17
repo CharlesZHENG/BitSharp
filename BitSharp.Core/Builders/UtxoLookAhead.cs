efaee48796515b3f9aa00d6abd7a00b8619f87c3
Paul <pmlyon@hotmail.ca>
2015-07-17 10:03:37 -0300

    use SplitCombineBlock for utxo look ahead ordering

--- BitSharp.Core/Builders/UtxoLookAhead.cs
+++ BitSharp.Core/Builders/UtxoLookAhead.cs
@@ -18,13 +18,19 @@ namespace BitSharp.Core.Builders
 
         public static ISourceBlock<BlockTx> LookAhead(ISourceBlock<BlockTx> blockTxes, IDeferredChainStateCursor deferredChainStateCursor, CancellationToken cancelToken = default(CancellationToken))
         {
-            var pendingWarmedTxes = new ConcurrentQueue<Tuple<BlockTx, CompletionCount>>();
+            TransformBlock<BlockTx, BlockTx> blockTxSplitter; TransformManyBlock<BlockTx, BlockTx> blockTxCombiner;
+            SplitCombineBlock.Create<BlockTx, BlockTx, int>(
+                blockTx => blockTx.Index,
+                blockTx => blockTx.Index,
+                out blockTxSplitter, out blockTxCombiner,
+                cancelToken);
+
+            blockTxes.LinkTo(blockTxSplitter, new DataflowLinkOptions { PropagateCompletion = true });
 
             // queue each utxo entry to be warmed up: each input's previous transaction, and each new transaction
-            var queueUnspentTxLookup = InitQueueUnspentTxLookup(pendingWarmedTxes, cancelToken);
+            var queueUnspentTxLookup = InitQueueUnspentTxLookup(cancelToken);
 
-            // link the block txes to the unspent tx queuer
-            blockTxes.LinkTo(queueUnspentTxLookup, new DataflowLinkOptions { PropagateCompletion = true });
+            blockTxSplitter.LinkTo(queueUnspentTxLookup, new DataflowLinkOptions { PropagateCompletion = true });
 
             // warm up a uxto entry
             var warmupUtxo = InitWarmupUtxo(deferredChainStateCursor, cancelToken);
@@ -32,34 +38,30 @@ namespace BitSharp.Core.Builders
             // link the utxo entry queuer to the warmer
             queueUnspentTxLookup.LinkTo(warmupUtxo, new DataflowLinkOptions { PropagateCompletion = true });
 
-            // forward any warmed txes, in order
-            var forwardWarmedTxes = InitForwardWarmedTxes(pendingWarmedTxes, cancelToken);
-
             // link notification of a warmed tx to the in-order forwarder
-            warmupUtxo.LinkTo(forwardWarmedTxes, new DataflowLinkOptions { PropagateCompletion = true });
+            warmupUtxo.LinkTo(blockTxCombiner, new DataflowLinkOptions { PropagateCompletion = true });
 
-            return forwardWarmedTxes;
+            return blockTxCombiner;
         }
 
-        private static TransformManyBlock<BlockTx, Tuple<UInt256, CompletionCount>> InitQueueUnspentTxLookup(ConcurrentQueue<Tuple<BlockTx, CompletionCount>> pendingWarmedTxes, CancellationToken cancelToken)
+        private static TransformManyBlock<BlockTx, Tuple<UInt256, CompletionCount, BlockTx>> InitQueueUnspentTxLookup(CancellationToken cancelToken)
         {
-            return new TransformManyBlock<BlockTx, Tuple<UInt256, CompletionCount>>(
+            return new TransformManyBlock<BlockTx, Tuple<UInt256, CompletionCount, BlockTx>>(
                 blockTx =>
                 {
                     var inputCount = !blockTx.IsCoinbase ? blockTx.Transaction.Inputs.Length : 0;
                     var completionCount = new CompletionCount(inputCount + 1);
-                    pendingWarmedTxes.Enqueue(Tuple.Create(blockTx, completionCount));
 
-                    var txHashes = new Tuple<UInt256, CompletionCount>[inputCount + 1];
+                    var txHashes = new Tuple<UInt256, CompletionCount, BlockTx>[inputCount + 1];
 
-                    txHashes[0] = Tuple.Create(blockTx.Hash, completionCount);
+                    txHashes[0] = Tuple.Create(blockTx.Hash, completionCount, blockTx);
 
                     if (!blockTx.IsCoinbase)
                     {
                         for (var inputIndex = 0; inputIndex < blockTx.Transaction.Inputs.Length; inputIndex++)
                         {
                             var txHash = blockTx.Transaction.Inputs[inputIndex].PreviousTxOutputKey.TxHash;
-                            txHashes[inputIndex + 1] = Tuple.Create(txHash, completionCount);
+                            txHashes[inputIndex + 1] = Tuple.Create(txHash, completionCount, blockTx);
                         }
                     }
 
@@ -68,43 +70,23 @@ namespace BitSharp.Core.Builders
                 new ExecutionDataflowBlockOptions { CancellationToken = cancelToken });
         }
 
-        private static TransformManyBlock<Tuple<UInt256, CompletionCount>, object> InitWarmupUtxo(IDeferredChainStateCursor deferredChainStateCursor, CancellationToken cancelToken)
+        private static TransformManyBlock<Tuple<UInt256, CompletionCount, BlockTx>, BlockTx> InitWarmupUtxo(IDeferredChainStateCursor deferredChainStateCursor, CancellationToken cancelToken)
         {
-            return new TransformManyBlock<Tuple<UInt256, CompletionCount>, object>(
+            return new TransformManyBlock<Tuple<UInt256, CompletionCount, BlockTx>, BlockTx>(
                 tuple =>
                 {
                     var txHash = tuple.Item1;
                     var completionCount = tuple.Item2;
+                    var blockTx = tuple.Item3;
 
                     deferredChainStateCursor.WarmUnspentTx(txHash);
 
-                    // return a 1 element to trigger ForwardWarmedTxes
                     if (completionCount.TryComplete())
-                        return new object[1];
+                        return new[] { blockTx };
                     else
-                        return new object[0];
+                        return new BlockTx[0];
                 },
                 new ExecutionDataflowBlockOptions { CancellationToken = cancelToken, MaxDegreeOfParallelism = deferredChainStateCursor.CursorCount });
-        }
-
-        private static TransformManyBlock<object, BlockTx> InitForwardWarmedTxes(ConcurrentQueue<Tuple<BlockTx, CompletionCount>> pendingWarmedTxes, CancellationToken cancelToken)
-        {
-            return new TransformManyBlock<object, BlockTx>(
-                _ =>
-                {
-                    // return any fully warmed txes, preserving the tx order
-                    var warmedTxes = new List<BlockTx>();
-
-                    Tuple<BlockTx, CompletionCount> tuple;
-                    while (pendingWarmedTxes.TryPeek(out tuple) && tuple.Item2.IsComplete)
-                    {
-                        warmedTxes.Add(tuple.Item1);
-                        pendingWarmedTxes.TryDequeue(out tuple);
-                    }
-
-                    return warmedTxes;
-                },
-                new ExecutionDataflowBlockOptions { CancellationToken = cancelToken });
         }
     }
 }
