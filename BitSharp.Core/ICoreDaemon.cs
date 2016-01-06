d5de92374fdb77d63a2eb11e7f06a87ae082e003
Paul <pmlyon@hotmail.ca>
2016-01-06 18:21:56 -0400

    fix stats worker following the wrong chain for unconfirmed txes

--- BitSharp.Core/ICoreDaemon.cs
+++ BitSharp.Core/ICoreDaemon.cs
@@ -18,6 +18,8 @@ namespace BitSharp.Core
         /// </summary>
         Chain CurrentChain { get; }
 
+        Chain UnconfirmedTxesChain { get; }
+
         IChainState GetChainState();
     }
 }
